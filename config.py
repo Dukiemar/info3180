421e28c6ae47df20f8486c44525b4e5332c7c9ce
dukiemar <dukiemarshaw@gmail.com>
2015-09-06 21:29:12 +0000

    first commit

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = 'javanddukes'
 
 #the updates databases are below
-SQLALCHEMY_DATABASE_URI='postgresql://dukiemar:rameik@localhost/mydatabase'
-#SQLALCHEMY_DATABASE_URI="postgresql://vipywuddzcwghu:TD-iEfkQMgBDcthqFpwlV7MjBD@ec2-54-197-245-93.compute-1.amazonaws.com:5432/ddjb4h7m7opmpk"
+#SQLALCHEMY_DATABASE_URI='postgresql://dukiemar:rameik@localhost/mydatabase'
+SQLALCHEMY_DATABASE_URI="postgresql://vipywuddzcwghu:TD-iEfkQMgBDcthqFpwlV7MjBD@ec2-54-197-245-93.compute-1.amazonaws.com:5432/ddjb4h7m7opmpk"
